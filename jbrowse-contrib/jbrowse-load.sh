a5b7b5783ccadd09cc9e31c0cb6f073a23e2ba9a
Vivek Krishnakumar <vkrishna@jcvi.org>
2015-10-14 14:12:05 -0400

    + Add Araport11 track config files + Update core TAIR10 + community tracks config file + Add VISTA tracks served from Phytozome VISTA REST API (proxied via ADAMA) + Add helper functions to color VISTA tracks + Fix variables used in jbrowse load shell script

--- jbrowse-contrib/jbrowse-load.sh
+++ jbrowse-contrib/jbrowse-load.sh
@@ -14,86 +14,88 @@ JBROWSE_DATA=data/json/arabidopsis
 #    --trackLabel "TAIR10_genome" --key "Reference Sequence"
 
 # Genes
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
     --trackLabel "TAIR10_loci" --key "Locus" --nameAttributes "name,alias,id,symbol" \
     --trackType "JBrowse/View/Track/CanvasFeatures" --type "gene,transposable_element_gene,pseudogene" --noSubfeatures
 
-./bin/flatfile-to-json.pl --out data/json/arabidopsis/ --gff /usr/local/projects/AIP/DataProviders/TAIR/custom_data/ftp.arabidopsis.org/Genes/TAIR10_genome_release/TAIR10_gff3/TAIR10_GFF3_genes_transposons.AIP.gff --key "Protein Coding Gene Models" --nameAttributes "name,alias,id,symbol" --trackType "JBrowse/View/Track/HTMLFeatures" --type "mRNA" --trackLabel "TAIR10_genes"
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
+    --key "Protein Coding Gene Models" --nameAttributes "name,alias,id,symbol" --trackType "JBrowse/View/Track/HTMLFeatures" \
+    --type "mRNA" --trackLabel "TAIR10_genes"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
     --trackLabel "TAIR10_transposons" --key "Natural Transposons" --type "transposable_element:TAIR10" \
     --className "generic_parent" --subfeatureClasses '{"transposon_fragment": "feature3"}' \
     --nameAttributes "name,alias,id,symbol"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR10_GFF3/TAIR10_unconfirmed_exons.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_GFF3/TAIR10_unconfirmed_exons.gff \
     --trackLabel "TAIR10_unconfirmed_exons" --key "Unconfirmed Exons" --className "exon" \
     --type "exon:TAIREXONCONF" --nameAttributes "name"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
     --trackLabel "TAIR10_pseudogenes" --key "Pseudogenes" --className "generic_parent" \
     --subfeatureClasses '{"pseduogenic_exon":"exon"}' --type "pseudogenic_transcript:TAIR10" \
     --nameAttributes "name,alias,id,symbol"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR10_GFF3/Spliced_Junctions_clustered.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_GFF3/Spliced_Junctions_clustered.gff \
     --trackLabel "TAIR10_Spliced_Junctions_clustered" --key "Spliced Junctions" \
     --className "exon" --type "exon:SAM1" --nameAttributes "name"
 
 bin/flatfile-to-json.pl --out $JBROWSE_DATA \
-    --gff $TAIR9_CUSTOM_GFF3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff \
+    --gff $AIP_HOME/$TAIR_DATA/$TAIR9_CUSTOM_GFF3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff \
     --trackLabel "TAIR9_Quesneville_Transposons" --key "Quesneville et al. Natural Transposons" \
     --type "transposable_element:Quesneville" --className "generic_parent" \
     --subfeatureClasses '{"transposon_fragment": "match_part"}'
 
 # Genomic Features
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_CUSTOM_GFF3/TAIR10_GFF3_genes_transposons.AIP.gff \
     --trackLabel "TAIR10_ncRNAs" --key "ncRNAs" --nameAttributes "name,alias,id,symbol" \
     --type "ncRNA:TAIR10,miRNA:TAIR10,tRNA:TAIR10,snoRNA:TAIR10,snRNA:TAIR10,rRNA:TAIR10" \
     --className "generic_parent" --subfeatureClasses '{"exon": "feature2"}'
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR10_GFF3/TAIR_GFF3_ssrs.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR10_GFF3/TAIR_GFF3_ssrs.gff \
     --trackLabel "TAIR_SSRs" --key "Tandem Repeats" --className "feature3" --nameAttributes "name" \
     --clientConfig '{"featureCss": "background-color: #623;"}' --type "satellite:TandemRepeatsFinder_v4.04"
 
 # Assembly tracks
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR9_GFF3/Assembly_GFF/TAIR9_GFF3_assemblies.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR9_GFF3/Assembly_GFF/TAIR9_GFF3_assemblies.gff \
     --trackLabel "TAIR9_assemblies" --key "Tiling Path" --className "feature5" \
     --type "BAC_cloned_genomic_insert:TAIR" --nameAttributes "name"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR9_GFF3/Assembly_GFF/tair9_Assembly_gaps.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR9_GFF3/Assembly_GFF/tair9_Assembly_gaps.gff \
     --trackLabel "TAIR9_assembly_gaps" --key "Gaps" --className "feature5" \
     --clientConfig '{"featureCss": "background-color: red;"}' --type "gap:TAIR9"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR9_GFF3/Assembly_GFF/tair9_assembly_updates.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR9_GFF3/Assembly_GFF/tair9_assembly_updates.gff \
     --trackLabel "TAIR9_assembly_updates" --key "Assembly Updates" --className "feature3" \
     --type "possible_assembly_error:TAIR9,possible_base_call_error:TAIR9,possible_deletion:TAIR9,possible_insertion:TAIR9"
 
 # Variation
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR9_GFF3/Variation_GFF/TAIR9_GFF3_tdnas.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR9_GFF3/Variation_GFF/TAIR9_GFF3_tdnas.gff \
     --trackLabel "TAIR9_tDNAs" --key "T-DNAs/Transposons" \
     --type "transposable_element_insertion_site:TAIR9" --noSubfeatures \
     --className "triangle hgred invert" --nameAttributes "name"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR9_GFF3/Variation_GFF/TAIR9_GFF3_polymorphisms.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR9_GFF3/Variation_GFF/TAIR9_GFF3_polymorphisms.gff \
     --trackLabel "TAIR9_polymorphisms" --key "Polymorphisms" --nameAttributes "name" \
     --type "INDEL:Allele,compound:Allele,deletion:Allele,insertion:Allele,substitution:Allele" \
     --className "diamond dmyellow" --nameAttributes "name"
 
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR9_GFF3/Variation_GFF/TAIR9_GFF3_markers.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR9_GFF3/Variation_GFF/TAIR9_GFF3_markers.gff \
     --trackLabel "TAIR9_markers" --key "Markers" --type "marker:TAIR9" --className "generic_part_a" \
     --nameAttributes "name"
 
 # Expression
-bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $TAIR9_GFF3/Expression_GFF/tair9_ATH1-121501.gff \
+bin/flatfile-to-json.pl --out $JBROWSE_DATA --gff $AIP_HOME/$TAIR_DATA/$TAIR9_GFF3/Expression_GFF/tair9_ATH1-121501.gff \
     --trackLabel "TAIR9_Microarray_probes" --key "Microarray ATH1-121501 probes" \
     --type "microarray_probe:NetAffx" --className "generic_part_a" --nameAttributes "name"
 
 bin/flatfile-to-json.pl --out $JBROWSE_DATA \
-    --gff $TAIR9_CUSTOM_GFF3/Expression_GFF/tair9_atproteometair7.gff --trackLabel "TAIR9_AtProteome" \
+    --gff $AIP_HOME/$TAIR_DATA/$TAIR9_CUSTOM_GFF3/Expression_GFF/tair9_atproteometair7.gff --trackLabel "TAIR9_AtProteome" \
     --key "AtProteome (Baerenfaller et al. 2008)" --type "protein_match:AtProteomeTAIR9" \
     --className "generic_parent" --subfeatureClasses '{"match_part": "feature5"}'
 
 bin/flatfile-to-json.pl --out $JBROWSE_DATA \
-    --gff $TAIR9_CUSTOM_GFF3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
+    --gff $AIP_HOME/$TAIR_DATA/$TAIR9_CUSTOM_GFF3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
     --trackLabel "TAIR9_AtPeptide" --key "AtPeptide (Castellana et al. 2008)" \
     --type "protein_match:at_proteome_TAIR9" --className "generic_parent" \
     --subfeatureClasses '{"match_part": "feature5"}'
